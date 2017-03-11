2ce52d5615b9da48cc86851ca4ef14a989c0fd21
Alexander Migai <mig-alex14@mail.ru>
2017-03-11 21:05:52 +0300

    new Vers

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -5,7 +5,6 @@ import com.sun.xml.internal.ws.api.client.SelectOptimalEncodingFeature;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Hi!");
         System.out.println(" Work!");
     }
